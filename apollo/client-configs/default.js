762c735b98954f872957af7c0041fc902a80e704
Dhaval Vyas <dhavs1989@gmail.com>
2021-07-15 01:44:01 +0000

    Changes for GraphCMS

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -1,6 +1,6 @@
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-const GRAPHCMS_API = 'https://api-useast.graphcms.com/v1/cjm5gebne167p01c4x2mauv65/master'
+const GRAPHCMS_API = 'https://api-us-east-1.graphcms.com/v2/cjm5gebne167p01c4x2mauv65/master'
 
 export default () => ({
   link: new HttpLink({ uri: GRAPHCMS_API }),
